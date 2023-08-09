9082bda75e9f74567325bfad080f77f08149b98c
Divakar <132250823+divakargmx@users.noreply.github.com>
2023-08-09 20:12:54 +0530

    Update bg_remove.py

--- bg_remove.py
+++ bg_remove.py
@@ -1,17 +1,18 @@
-import streamlit as st
-from rembg import remove
-from PIL import Image
 from io import BytesIO
-import base64
+
+import streamlit as st
+from PIL import Image
+from rembg import remove
 
 st.set_page_config(layout="wide", page_title="Image Background Remover")
 
 st.write("## Remove background from your image")
 st.write(
-    ":dog: Try uploading an image to watch the background magically removed. Full quality images can be downloaded from the sidebar. "
+    ":dog: Try uploading an image to watch the background magically removed. Full quality images can be downloaded from the sidebar. This code is open source and available [here](<https://github.com/tyler-simons/BackgroundRemoval>) on GitHub. Special thanks to the [rembg library](<https://github.com/danielgatis/rembg>) :grin:"
 )
 st.sidebar.write("## Upload and download :gear:")
 
+# Create the columns
 col1, col2 = st.columns(2)
 
 # Download the fixed image
@@ -21,7 +22,7 @@ def convert_image(img):
     byte_im = buf.getvalue()
     return byte_im
 
-
+# Package the transform into a function
 def fix_image(upload):
     image = Image.open(upload)
     col1.write("Original Image :camera:")
@@ -30,12 +31,15 @@ def fix_image(upload):
     fixed = remove(image)
     col2.write("Fixed Image :wrench:")
     col2.image(fixed)
-    st.sidebar.markdown("\n")
-    st.sidebar.download_button("Download fixed image", convert_image(fixed), "fixed.png", "image/png")
+    st.sidebar.markdown("\\n")
+    st.sidebar.download_button(
+        "Download fixed image", convert_image(fixed), "fixed.png", "image/png"
+    )
 
-
+# Create the file uploader
 my_upload = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
 
+# Fix the image!
 if my_upload is not None:
     fix_image(upload=my_upload)
 else:
